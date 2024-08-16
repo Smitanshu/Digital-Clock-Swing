91791fe239f6b0cd44b025eb0f3fe94a321e0724
CraftSyntax <150527585+Smitanshu@users.noreply.github.com>
2024-08-16 11:46:33 +0530

    Succesfully build in desired format

---
+++ src/com/clock/MyWindow.java
@@ -0,0 +1,98 @@
+package com.clock;
+
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame {
+
+
+
+    private JLabel  heading;
+    private JLabel clockLabel;
+
+
+    private Font font =new Font("", Font.BOLD,35);
+
+    public MyWindow(){
+
+
+
+
+        super.setTitle("My Clock");
+        //If not used super then no problem then it understand that it is the method of the parent.
+        super.setSize(400 ,400);
+        super.setLocation(300,50);
+        this.createGui();
+        this.startClock();
+        super.setVisible(true);
+    }
+
+    private void  createGui(){
+
+        //Gui:Graphical User Interface
+        heading =new JLabel("My Clock");
+        clockLabel=new  JLabel("clock");
+        heading.setFont(font);
+        clockLabel.setFont(font);
+
+        this.setLayout(new GridLayout(2,1));
+        this.add(heading);
+        this.add(clockLabel);
+
+
+
+
+    }
+
+public void startClock(){
+
+//        Timer timer=new Timer(1000, new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//             //  String dateTime=new Date().toString();
+//               // String dateTime=new Date().toLocaleString();
+//
+//                Date d=new Date();
+//
+//                SimpleDateFormat sfd=new SimpleDateFormat("hh:mm:ss a");
+//                String dateTime=sfd.format(d);
+//
+//               clockLabel.setText(dateTime);
+//
+//
+//            }
+//        });
+//        timer.start();
+
+        Thread t=new Thread(){
+            public void run(){
+                try{
+
+                    while (true){
+                        Date d=new Date();
+
+                        SimpleDateFormat sfd=new SimpleDateFormat("hh:mm:ss a");
+                        String dateTime=sfd.format(d);
+
+                        clockLabel.setText(dateTime);
+                       // Thread.sleep(1000);
+                        Thread.currentThread().sleep(1000);
+
+
+
+                    }
+
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+
+                }
+            }
+        };
+
+   t.start();
+}
+}
